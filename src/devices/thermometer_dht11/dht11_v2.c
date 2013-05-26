e7737a33f0917d0bb4bde8da666f480078c298fa
Peter Vojtek <peter.vojtek@gmail.com>
2013-05-26 19:43:33 +0200

    update thermometer DHT11 C code to fit our ruby code

--- src/devices/thermometer_dht11/dht11_v2.c
+++ src/devices/thermometer_dht11/dht11_v2.c
@@ -3,6 +3,12 @@
  *      dht11_v2.c.
  *      GPIO Thermometer DHT11
  *	This is example code from the following blog: http://rpidude.blogspot.sk/2012/11/temp-sensor-and-wiringpi.html
+ *      I made few minor modifications to better suit the code to my app:
+ *        - the stdout temperature/humidity is modified
+ *        - measurement is not called in loop, but only once
+ * 
+ *     compile: gcc -o dht11_v2 dht11_v2.c -L/usr/local/lib -lwiringPi
+ *     connect the device: https://github.com/sk-vpohybe/stopa-monitor/wiki/GPIO-Thermometer-Hygrometer-DHT11
  */
 
 #include <wiringPi.h>
@@ -61,29 +67,20 @@ void read_dht11_dat()
   // print it out if data is good
   if ((j >= 40) && 
       (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)) ) {
-    f = dht11_dat[2] * 9. / 5. + 32;
-    printf("Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n", 
-            dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
+    printf("Humidity:%d.%d\nTemperature:%d.%d\n", 
+            dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
   }
   else
   {
-    printf("Data not good, skip\n");
+    exit (2) ;
   }
 }
 
 int main (void)
 {
-
-  printf ("Raspberry Pi wiringPi DHT11 Temperature test program\n") ;
-
   if (wiringPiSetup () == -1)
     exit (1) ;
 
-  while (1) 
-  {
-     read_dht11_dat();
-     delay(1000); // wait 1sec to refresh
-  }
-
+  read_dht11_dat();
   return 0 ;
 }
