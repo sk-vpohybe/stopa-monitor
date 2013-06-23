6171ba56acceba52f7979251a29dda1c41acd9e5
Peter Vojtek <peter.vojtek@gmail.com>
2013-06-23 20:42:20 +0200

    fix bug: CameraCreativeLiveHD photos are over exposed

--- src/devices/camera_creative_live_hd.rb
+++ src/devices/camera_creative_live_hd.rb
@@ -1,7 +1,7 @@
 class CameraCreativeLiveHD
   include DaylightDetector
   
-  TIMEOUT = 15 #seconds
+  TIMEOUT = 30 #seconds
   OUTPUT_FILE = 'photo_creative_live_hd.jpg'
 
   def initialize logger, snapshot_dir
@@ -13,8 +13,14 @@ class CameraCreativeLiveHD
   def capture
     if daylight?(StopaMonitorConfig::LATITUDE, StopaMonitorConfig::LONGITUDE)
       begin
-        Timeout::timeout(15) do
-          @logger.debug `fswebcam --no-banner -d /dev/video0 -i 0 -r 1280x720 --jpeg 80 -D 1 --set sharpness=1 #{@output_file} 2>&1`
+        Timeout::timeout(TIMEOUT) do
+          # we intentionally take three photos to allow the camera to autoset brightness/color
+          cmd = 'fswebcam --no-banner -d /dev/video0 -i 0 -r 1280x720 --jpeg 80 -D 1 --set sharpness=1'
+          @logger.debug `#{cmd} /dev/null 2>&1`
+          sleep 2
+          @logger.debug `#{cmd} /dev/null 2>&1`
+          sleep 2
+          @logger.debug `#{cmd} #{@output_file} 2>&1`
         end
       rescue Timeout::Error
         @logger.error '15 second timeout expired while taking photo'
