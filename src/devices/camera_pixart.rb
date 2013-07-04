fb5877b02957433398ce6b9112d422b024f166f0
Peter Vojtek <peter.vojtek@gmail.com>
2013-07-04 17:18:53 +0200

    add device: usb webcam Pixart

---
+++ src/devices/camera_pixart.rb
@@ -0,0 +1,33 @@
+class CameraPixart
+  include DaylightDetector
+  
+  TIMEOUT = 15 #seconds
+  OUTPUT_FILE = 'photo_pixart.jpg'
+
+  def initialize logger, snapshot_dir
+    @logger = logger
+              
+    @output_file = File.join snapshot_dir, OUTPUT_FILE
+  end
+  
+  def capture
+    if daylight?(StopaMonitorConfig::LATITUDE, StopaMonitorConfig::LONGITUDE)
+      begin
+        Timeout::timeout(15) do
+          @logger.debug `fswebcam --no-banner -d /dev/video0 -i 0 -r 640x480 --jpeg 80 -D 1 #{@output_file} 2>&1`
+        end
+      rescue Timeout::Error
+        @logger.error '15 second timeout expired while taking photo'
+      end
+
+      if File.exists?(@output_file)
+        size = File.size @output_file
+        @logger.info "photo successfully taken: #{@output_file}. size: #{size} bytes"
+      else
+        @logger.error 'photo not created'
+      end
+    else
+      @logger.info "not taking photo, no daylight"
+    end
+  end
+end
